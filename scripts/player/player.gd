0ef0a02862ca963359d467d404b14ef7e119a4b4
tildozer <actninswitch@gmail.com>
2025-03-16 11:38:13 -0600

    added crashing and success partacles, then customized ship

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -8,24 +8,38 @@ extends RigidBody3D
 
 var is_transitioning: bool = false
 
+#Audio$explosionParticles
 @onready var explosion_audio: AudioStreamPlayer = $explosionAudio
 @onready var success_audio: AudioStreamPlayer = $successAudio
 @onready var rocket_thrust: AudioStreamPlayer3D = $rocketAudio
+#Particles
+@onready var thruster_particles: GPUParticles3D = $boosterParticles
+@onready var right_thruster_particles: GPUParticles3D = $rightBoosterParticles
+@onready var left_thruster_particles: GPUParticles3D = $leftBoosterParticles
+@onready var explosion_particles :GPUParticles3D = $explosionParticles
+@onready var success_particles :GPUParticles3D = $successParticles
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		thruster_particles.emitting = true
 		if !rocket_thrust.playing:
 			rocket_thrust.play()
 	else:
+		thruster_particles.emitting = false
 		rocket_thrust.stop()
 	
 	if Input.is_action_pressed("rotate_left"):
+		left_thruster_particles.emitting = true
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+	else: 
+		left_thruster_particles.emitting = false
 	
 	if Input.is_action_pressed("rotate_right"):
+		right_thruster_particles.emitting = true
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
-	
+	else:
+		right_thruster_particles.emitting = false
 
 func _on_body_entered(body: Node) -> void:
 	if !is_transitioning:
@@ -35,8 +49,9 @@ func _on_body_entered(body: Node) -> void:
 			call_deferred("crash_sequence")
 
 func crash_sequence() -> void:
-	rocket_thrust.stop()
+	explosion_particles.emitting = true
 	explosion_audio.play()
+	stop_thrust_effects()
 	is_transitioning = true
 	set_process(false)
 	var tween = create_tween()
@@ -44,8 +59,9 @@ func crash_sequence() -> void:
 	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
-	rocket_thrust.stop()
+	success_particles.emitting = true
 	success_audio.play()
+	stop_thrust_effects()
 	is_transitioning = true
 	set_process(false)
 	var tween = create_tween()
@@ -53,3 +69,7 @@ func complete_level(next_level_file: String) -> void:
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file)
 	)
+
+func stop_thrust_effects() -> void:
+	rocket_thrust.stop()
+	thruster_particles.emitting = false
